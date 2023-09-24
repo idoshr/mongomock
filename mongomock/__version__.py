17810f8585cc1ee3a647f948e301387bc314a654
Ido Shraga <35264146+idoshr@users.noreply.github.com>
2023-09-24 10:24:32 +0300

    Update __version__.py

--- mongomock/__version__.py
+++ mongomock/__version__.py
@@ -1,3 +1,8 @@
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution('mongomock').version
+if (int(python_version[0]) == 3 and int(python_version[1]) >= 8) or int(python_version[0]) >= 4:
+    from importlib.metadata import version
+    __version__ = version('mongomock')
+else:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution('mongomock').version
